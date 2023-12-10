a670690f5a674eaad29f29d565ac720ecb834bee
matoussynek <33132360+matoussynek@users.noreply.github.com>
2023-12-10 18:05:59 +0000

    10 star2

--- 10/code.py
+++ 10/code.py
@@ -71,32 +71,61 @@ class Map:
         return (current[0]+dir[0], current[1]+dir[1])
     
     def star1(self):
-        found_nodes = []
+        self.found_nodes = []
         current_position = self.start_position
         current_node = self.get_node(current_position)
         last_input = 2
         while True:
 
-            if len(found_nodes) > 0 and current_position == self.start_position:
+            if len(self.found_nodes) > 0 and current_position == self.start_position:
                 break
-
-            print('im at:', current_node.symbol)
 
             possible_outputs = current_node.get_outputs(last_input)
             for possible_input, possible_output in possible_outputs:
                 possible_node = self.get_node(self.get_position(current_position, possible_output))
                 if possible_node.has_input(possible_input):
                     current_node = possible_node
-                    found_nodes.append(possible_node)
+                    self.found_nodes.append(possible_node)
                     current_position = self.get_position(current_position, possible_output)
                     last_input = possible_input
                     break
         
-        print(f'Max distance: {len(found_nodes)//2}')
+        print(f'Max distance: {len(self.found_nodes)//2}')
+
+    def star2_crossings_in_dir(self, pos, dir):
+        crosses = 0
+        while pos[1] >= 0 and pos[1] < len(self.nodes) and pos[0] >= 0 and pos[0] < len(self.nodes[pos[1]]):
+            node = self.get_node(pos)
+            if node in self.found_nodes:
+                crosses += 1
+            pos = (pos[0]+dir[0], pos[1]+dir[1])
+        
+        return crosses
+
+    def star2_has_odd_crossings(self, pos):
+        is_odd = True
+        is_odd = is_odd and (self.star2_crossings_in_dir(pos, Directions.UP.value) % 2) == 1
+        is_odd = is_odd and (self.star2_crossings_in_dir(pos, Directions.RIGHT.value) % 2) == 1
+        is_odd = is_odd and (self.star2_crossings_in_dir(pos, Directions.DOWN.value) % 2) == 1
+        is_odd = is_odd and (self.star2_crossings_in_dir(pos, Directions.LEFT.value) % 2) == 1
+
+        return is_odd
+
+    
+    def star2(self):
+        tot = 0
+        for y,row in enumerate(self.nodes):
+            for x, c in enumerate(row):
+                if c in self.found_nodes:
+                    continue
+                if self.star2_has_odd_crossings((x,y)):
+                    tot += 1
+        
+        print(f'Total enclosed tiles: {tot}')
 
 
     
 map = Map(open('./input.txt').readlines())
 map.star1()
-
-#Wont bother with star2... No time
+#this doesnt work, i dont feel like working on this more...
+map.star2()
